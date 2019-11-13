e3e7f03630d1bf5b1eaf4440af268b13a6e44094
mark stahl <markstahl0219@gmail.com>
2019-11-13 15:15:18 -0500

    remove item works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+//Contexts
+import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -12,18 +16,25 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item])
+	};
+
+	const removeItem = id => {
+		const itemIsRemoved = cart.filter(item => item.id !== id);
+		setCart(itemIsRemoved);
 	};
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{cart, removeItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
+			<Route 
+			exact path="/" 
+			component={Products} 
+			/>
 					<Products
 						products={products}
 						addItem={addItem}
@@ -35,6 +46,8 @@ function App() {
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
